c77902c1f504408b98be8361b34f8dd2cb90bc0e
Boris Adamyan <boris.adamyan@gmail.com>
2018-12-18 17:55:45 +0400

    /js to js

--- assets/app/messages/message.model.ts
+++ assets/app/messages/message.model.ts
@@ -1,10 +1,10 @@
-export class  Message {
+export class Message {
     content: string;
     username: string;
     messageId?: string;
     userId?: string;
 
-    constructor(public content:string, public username:string, public messageId?:string, public userId?:string){
+    constructor(private content: string, private username: string, private messageId?: string, private userId?: string) {
         this.content = content;
         this.username = username;
         this.userId = userId;
